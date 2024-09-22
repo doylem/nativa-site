f0bd72b8f5f0537acf26192f99a8f003701d6da7
Matt Doyle <mattdoyl@gmail.com>
2024-09-22 15:34:10 +1000

    Add runtime path

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   output: "export", // <=== enables static exports
   basePath: "/nativa-site",
   assetPrefix: "/nativa-site",
+  publicRuntimeConfig: {
+    basePath: "/nativa-site",
+  },
   reactStrictMode: true,
 }
 
