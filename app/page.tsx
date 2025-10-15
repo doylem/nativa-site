20e0797fd6536d941a1c5ad6066549da542ae65f
Matthew Doyle <m.doyle@easygo.io>
2025-10-16 09:43:18 +1100

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function NativaLandingPage() {
               height={568}
               alt="Nativa logo"
               className="object-center"
-              style={{ width: "600px", minWidth: "400px" }}
+              style={{ width: "500px", minWidth: "350px" }}
             />
           </div>
           <h1 className="text-4xl md:text-5xl font-playfair font-normal mb-6 tracking-wide">
