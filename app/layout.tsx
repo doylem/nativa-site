21cbae9bd9453b938a6c14ac8da1d03998e01bec
Matthew Doyle <m.doyle@easygo.io>
2025-10-16 12:04:10 +1100

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,18 +14,15 @@ export const metadata: Metadata = {
     "Elevating the medicinal cannabis industry in Australia and across the globe by fostering transparency, collaboration and excellence.",
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: '48x48' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
-    other: [
-      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
-    ],
   },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
