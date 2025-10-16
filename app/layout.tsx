75ed5dd84142d0a66ef311de9a57b60f78e6feec
Matthew Doyle <m.doyle@easygo.io>
2025-10-16 11:46:31 +1100

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,13 @@ export const metadata: Metadata = {
   title: "Nativa",
   description:
     "Elevating the medicinal cannabis industry in Australia and across the globe by fostering transparency, collaboration and excellence.",
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/images/nativa-logo-leaf-only.png', type: 'image/png' },
+    ],
+    apple: '/images/nativa-logo-leaf-only.png',
+  },
 }
 
 export default function RootLayout({
