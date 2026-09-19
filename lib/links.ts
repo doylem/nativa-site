937c7f60c26d3046429a0e0e40ca3770328b8699
Matt Doyle <mattdoyl@gmail.com>
2026-09-19 13:47:41 +1000

    Add tally link

--- lib/links.ts
+++ lib/links.ts
@@ -1,8 +1,7 @@
 /** Google Form behind the "Get in Touch" button on the homepage. */
-export const CONTACT_FORM_URL = "https://forms.gle/9vALopPJLs3CSjYPA"
+export const CONTACT_FORM_URL = "https://forms.gle/9vALopPJLs3CSjYPA";
 
 /**
  * Tally form behind the ElevatED "Register" button.
- * TODO: replace with the real Tally share link once the form exists.
  */
-export const ELEVATED_REGISTER_URL = "https://tally.so/r/REPLACE_ME"
+export const ELEVATED_REGISTER_URL = "https://tally.so/r/2E2OJp";
